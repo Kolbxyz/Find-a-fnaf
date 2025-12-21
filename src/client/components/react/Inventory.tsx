fad4c5d2fcb7a8370e956f7f835672e5902185ca
Kolbxyz <Kolbxyz@outlook.com>
2025-12-21 14:29:31 +0100

    fix(ui): add missing key prop to Inventory list items

--- src/client/components/react/Inventory.tsx
+++ src/client/components/react/Inventory.tsx
@@ -5,7 +5,7 @@ export function Inventory({ items, onBack }: { items: string[]; onBack: () => vo
 		<frame Size={UDim2.fromScale(0.5, 0.5)} Position={UDim2.fromScale(0.25, 0.25)}>
 			<uilistlayout FillDirection="Vertical" Padding={new UDim(0, 5)} />
 			{items.map((item, i) => (
-				<textlabel Text={item} LayoutOrder={i + 1} Size={UDim2.fromScale(1, 0.1)} />
+				<textlabel key={i} Text={item} LayoutOrder={i + 1} Size={UDim2.fromScale(1, 0.1)} />
 			))}
 
 			<textbutton Text="Back" Size={UDim2.fromScale(1, 0.1)} Event={{ Activated: onBack }} />
